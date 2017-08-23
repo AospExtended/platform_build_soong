3e955d4f431f6002042bcdd925bc614f60978697
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with AOSP board flags
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"aosp/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -85,6 +87,9 @@ type variableProperties struct {
 			Cflags   []string
 			Cppflags []string
 		}
+
+		// include AOSP variables
+		*android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -150,6 +155,9 @@ type productVariables struct {
 	BtConfigIncludeDir *string `json:",omitempty"`
 
 	Override_rs_driver *string `json:",omitempty"`
+
+	// include AOSP variables
+	*android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
